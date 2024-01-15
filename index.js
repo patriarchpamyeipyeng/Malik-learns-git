63aa3ef7688a367b056e788954e12910bd1c3aa9
patriarchpam <patriarchpam700@gmail.com>
2024-01-15 15:41:51 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,45 @@
+alert("Welcome to my Fashion Hub!")
+
+// let x = 5
+// let y = 2
+
+// // if (x > y) {
+// //     write ("<h1>HI BUBU</h1>")
+// // }
+// // return ("BUBU")
+// let z = x + y
+
+// function addEventListener(params) {
+
+//     ("<h1>Hello there</h1>")
+    
+// }
+// addEventListener
+//  let hub = 100
+
+//  while (hub < 110) {
+//     console.log("This is beautiful");
+//     hub++ ;
+
+//  }
+ 
+
+//  document.write("OKAY")
+
+// for (let age = 5; age < 10; age++){
+//     console.log("You are older");
+
+// }
+// document.write("youre an adult!")
+
+let links = document.getElementsByTagName("a");
+
+for (let i = 1; i <= links.length; i++) {
+
+
+    console.log("this is link number" + i);
+    
+}
+document.write("all links now looped");
+
+   
